2c009c80b2b426c7a4d8011740e0f50338f67683
John Carreno <inglizcano@gmail.com>
2026-02-11 16:53:44 +0800

    Deploy 4:53pm 11-Feb-2026

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -18,10 +18,10 @@ const prismaClientSingleton = () => {
     })
 }
 
-declare global {
-    var prisma: undefined | ReturnType<typeof prismaClientSingleton>
+const globalForPrisma = globalThis as unknown as {
+    prisma: ReturnType<typeof prismaClientSingleton> | undefined
 }
 
-export const prisma = globalThis.prisma ?? prismaClientSingleton()
+export const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
 
-if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
